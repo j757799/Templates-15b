2a80fe3524b833ea52c9bb8e154c5319b60e5692
jesse.picker@gmail.com <jesse.picker@gmail.com>
2021-05-02 17:32:44 -0400

    Add project files.

---
+++ Templates/Templates.cpp
@@ -0,0 +1,22 @@
+//Name: Jesse Picker
+//Date: 5/2/2021
+//Project Name: Templates 15b
+//Project Description: Templated functions perform math on varying numeric data types.
+
+//Libraries
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+//Program Structure
+using namespace std;
+
+int main() 
+{
+	
+	
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
