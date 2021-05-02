3443403a4a9d0ef6b23a9c32f905e2e13cbc276f
jesse.picker@gmail.com <jesse.picker@gmail.com>
2021-05-02 18:50:47 -0400

    Almost done

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -1,7 +1,7 @@
 //Name: Jesse Picker
 //Date: 5/2/2021
 //Project Name: Templates 15b
-//Project Description: Templated functions perform math on varying numeric data types.
+//Project Description: Templated functions perform math on varying numeric data types
 
 //Libraries
 #include<iostream>
@@ -12,22 +12,17 @@
 using namespace std;
 
 template <class R>
-R half(R value)
+int half(R value)
 {
-	float mine2 = static_cast<float>(mine);
-	cout << "\n\nHere is half the value of the int: " << (round((mine2 / 2))) << endl;
+	float mine2 = static_cast<float>(value);
+	return (round((mine2 / 2)));
 }
 
-template <class R>
-R half(R value1, R value2)
+template <class R1, class R2>
+double half(R1 value1, R2 value2)
 {
-
+	return (value1 / 2);
 }
-
-//void half(int);
-//void half(float); //has 7 digits
-//void half(double); //has 15
-
 
 int main() 
 {
@@ -36,18 +31,15 @@ int main()
 	float myChoiceFLT = 3.14159;
 	double myChoiceDBL = 3.14159265358979323846;
 
-	half(myChoiceINT);
+	cout << "The result of halving (and rounding of course) my integer is: " << half(myChoiceINT);
 
-	
+	cout << fixed << showpoint << setprecision(5);
+	cout << "\n\nThe result of halving my float to 5 decimal places is: " << half(myChoiceFLT, myChoiceDBL);
+
+	cout << fixed << showpoint << setprecision(10);
+	cout << "\n\nThe result of halving my double to 10 decimal places is: " << half(myChoiceDBL, myChoiceFLT);
 
 	cout << endl << endl;
 	system("pause");
 	return 0;
 }
-
-//void half(int mine)
-//{
-//	if ()
-//	float mine2 = static_cast<float>(mine);
-//	cout << "\n\nHere is half the value of the int: " << (round((mine2 / 2))) << endl;
-//}
